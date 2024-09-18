54a4642bc9487f0d569b57678a6f9693e822eb8e
Fahad Khan <fahad.khan44604@gmail.com>
2024-09-18 21:46:13 +0530

    "fixed imports again

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../lib/authOptions";
+import { authOptions } from "@/app/libs/authOptions";
 
 export default async function getSession() {
   return await getServerSession(authOptions);
