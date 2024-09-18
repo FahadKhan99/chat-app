182dda425b3a597c2ea5fbf892228d7aa634053f
Fahad Khan <fahad.khan44604@gmail.com>
2024-09-18 19:55:15 +0530

    "fixed"

--- app/libs/authOptions.ts
+++ app/libs/authOptions.ts
@@ -1,12 +1,12 @@
 import bcrypt from "bcrypt";
-import { AuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./prismaClient";
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
