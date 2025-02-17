f8deccbfda27dc2779d03700b4e171aea7856f2c
Fahad Khan <fahad.khan44604@gmail.com>
2025-02-18 03:19:07 +0530

    Changed DB to neon postgressql

--- app/libs/pusher.js
+++ app/libs/pusher.js
@@ -2,16 +2,16 @@ import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  secret: process.env.PUSHER_SECRET!,
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+  secret: process.env.PUSHER_SECRET,
   cluster: "ap2",
   useTLS: true,
 });
 
 // use cluster eu
 export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
   {
     channelAuthorization: {
       endpoint: "/api/pusher/auth",
